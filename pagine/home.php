cce1f9566cd9385ff3663a59efa4a5e87cb3fdc3
Khuba001 <jakublewinski2701@gmail.com>
2024-06-21 22:05:18 +0200

    to polish

--- pagine/home.php
+++ pagine/home.php
@@ -11,7 +11,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <title>Biblioteca - Home</title>
+    <title>Biblioteka - Strona główna</title>
     <?php
     session_start();
     if (!isset($_SESSION['username'])) {
@@ -28,23 +28,23 @@
 
 <body>
     <ul class="barra">
-        <li class="barra"><a class="barra" href="home.php?ordine">Home</a></li>
-        <li class="barra"><a class="barra" href="aggiungi.php">Aggiungi</a></li>
-        <li class="barra"><a class="barra" href="letture.php">Letture</a></li>
-        <li class="barra" style="float: right;"><a class="barra" href="logout.php">Logout</a></li>
+        <li class="barra"><a class="barra" href="home.php?ordine">Strona główna</a></li>
+        <li class="barra"><a class="barra" href="aggiungi.php">Dodaj książkę</a></li>
+        <li class="barra"><a class="barra" href="letture.php">Wypożyczenia</a></li>
+        <li class="barra" style="float: right;"><a class="barra" href="logout.php">Wyloguj</a></li>
     </ul>
     <div id="titolo2">
-        <h1 class="aggiungi"><b>Biblioteca</b></h1>
+        <h1 class="aggiungi"><b>Biblioteka</b></h1>
     </div>
     <div class="btn-group btn-group-justified" style="width: 500px; margin: auto;">
-        <a href="home.php?ordine=titolo" class="btn btn-primary">Titolo</a>
-        <a href="home.php?ordine=autore" class="btn btn-primary">Autore</a>
-        <a href="home.php?ordine=genere" class="btn btn-primary">Genere</a>
+        <a href="home.php?ordine=titolo" class="btn btn-primary">Tytuł</a>
+        <a href="home.php?ordine=autore" class="btn btn-primary">Autor</a>
+        <a href="home.php?ordine=genere" class="btn btn-primary">Gatunek</a>
     </div>
     <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
         <div class="cerca">
             <div class="input-group">
-                <input type="text" class="form-control" name="ricerca" placeholder="Cerca titolo" autocomplete="off">
+                <input type="text" class="form-control" name="ricerca" placeholder="Szukaj tytułu" autocomplete="off">
                 <div class="input-group-btn">
                     <button class="btn btn-default" type="submit">
                         <i class="glyphicon glyphicon-search"></i>
@@ -61,46 +61,46 @@
                         WHERE titolo LIKE '%" . $_POST["ricerca"] . "%'";
         //echo $sql;
 
-        $ris = $conn->query($sql) or die("<p>Query fallita!</p>");
+        $ris = $conn->query($sql) or die("<p>Zapytanie nie powiodło się!</p>");
     } else if ($filtro == "titolo") {
         $conn = new mysqli($servername, $db_username, $db_password, $db_name);
         $sql = "SELECT codice, titolo, autore, genere, posizione
                     FROM libri
                     ORDER BY titolo";
 
-        $ris = $conn->query($sql) or die("<p>Query fallita! " . $conn->error . "</p>");
+        $ris = $conn->query($sql) or die("<p>Zapytanie nie powiodło się! " . $conn->error . "</p>");
     } else if ($filtro == "autore") {
         $conn = new mysqli($servername, $db_username, $db_password, $db_name);
         $sql = "SELECT codice, titolo, autore, genere, posizione
                     FROM libri
                     ORDER BY autore";
 
-        $ris = $conn->query($sql) or die("<p>Query fallita! " . $conn->error . "</p>");
+        $ris = $conn->query($sql) or die("<p>Zapytanie nie powiodło się! " . $conn->error . "</p>");
     } else if ($filtro == "genere") {
         $conn = new mysqli($servername, $db_username, $db_password, $db_name);
         $sql = "SELECT codice, titolo, autore, genere, posizione
                     FROM libri
                     ORDER BY genere";
 
-        $ris = $conn->query($sql) or die("<p>Query fallita! " . $conn->error . "</p>");
+        $ris = $conn->query($sql) or die("<p>Zapytanie nie powiodło się! " . $conn->error . "</p>");
     } else {
         $conn = new mysqli($servername, $db_username, $db_password, $db_name);
         $sql = "SELECT codice, titolo, autore, genere, posizione
                     FROM libri
                     ORDER BY codice";
 
-        $ris = $conn->query($sql) or die("<p>Query fallita! " . $conn->error . "</p>");
+        $ris = $conn->query($sql) or die("<p>Zapytanie nie powiodło się! " . $conn->error . "</p>");
     }
     ?>
     <div class="tabella">
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>Codice</th>
-                    <th>Titolo</th>
-                    <th>Autore</th>
-                    <th>Genere</th>
-                    <th>Posizione</th>
+                    <th>Kod</th>
+                    <th>Tytuł</th>
+                    <th>Autor</th>
+                    <th>Gatunek</th>
+                    <th>Pozycja</th>
                 </tr>
             </thead>
             <tbody>
